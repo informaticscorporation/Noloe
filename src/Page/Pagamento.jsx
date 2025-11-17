94c9bd2d1a70d2c78f214b68fda50f7e30fd0ea6
informaticscorporation <corporationinformatics@gmail.com>
2025-11-18 00:38:35 +0100

    ybdfnviododohsososo

--- src/Page/Pagamento.jsx
+++ src/Page/Pagamento.jsx
@@ -3,53 +3,31 @@ import { useLocation, useNavigate } from "react-router-dom";
 import "../UIX/Pagamento.css";
 
 export default function Pagamento() {
-  const location = useLocation();
+  
   const navigate = useNavigate();
-  const reservationData = location.state;
+  
 
   const [loading, setLoading] = useState(true);
   const [paymentUrl, setPaymentUrl] = useState(null);
 
  useEffect(() => {
-  if (!reservationData) return;
-
-  const initPayment = async () => {
-    try {
-     const response = await fetch("https://server-noloe.fly.dev/init-payment", {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json"
-  }
-});
+   async function fetchPaymentUrl() {
+     const response = await fetch("https://server-noloe.fly.dev/init-payment", )
+     const data = await response.json();
+     setPaymentUrl(data.url);
+     setLoading(false);
+   }
+}, []);
 
 
-      const data = await response.json();
-      setPaymentUrl(data.redirectURL);
-    } catch (err) {
-      console.error("Errore inizializzazione pagamento:", err);
-      alert("Errore durante l'inizializzazione del pagamento!");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  initPayment();
-}, [reservationData]);
-
-
-  if (!reservationData) {
-    return (
-      <div className="payment-container">
-        <h2>Nessuna prenotazione trovata ❌</h2>
-        <button className="btn-orange" onClick={() => navigate("/")}>
-          Torna alla Home
-        </button>
-      </div>
-    );
-  }
+ 
 
   return (
-    <div className="payment-container">
+    <div className="pagamento-container">
+      {loading ? (
+        <p>Loading payment...</p>
+      ) : (
+        <div className="payment-container">
       <button className="back-btn" onClick={() => navigate(-1)}>
         ← Indietro
       </button>
@@ -84,6 +62,8 @@ export default function Pagamento() {
             Simula pagamento completato ✅
           </button>
         </div>
+      )}
+    </div>
       )}
     </div>
   );
